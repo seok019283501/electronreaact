ac2728960b8abb600890c197b2b7321387d7ad16
seok019283501 <alsthr0338@gmail.com>
2023-05-23 20:17:45 +0900

    Movie app part one

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function App() {
-  const [toDo,setToDo] = useState("");
-  const [toDos,setToDos] = useState([]);
-  const onChange=(event)=>setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === ""){
-      return;
-    }
-    setToDos((currentArray)=>[toDo,...currentArray]);
-    setToDo("");
-  };
-  console.log(toDos);
-  return (
-    <div className="App">
-      <h1>My To Do ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type="text" placeholder="write your to do..."/>
-        <button>Add To Do</button>
-      </form>
-      <hr/>
-        {toDos.map((item,index)=><li key={index}>{item}</li>)}
-    </div>
-  );
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async() =>{
+      const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
+      const json = await response.json();
+      setMovies(json.data.movies);
+      setLoading(false);
+    };
+    useEffect(()=>{
+      getMovies();
+    },[])
+    return (
+      <div>
+        {loading ? <h1>Loading...</h1>: 
+        <div>
+          {movies.map((movie)=>(
+          <div key={movie.id}>
+            <img src={movie.medium_cover_image}/>
+            <h1>{movie.title}</h1>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.genres.map((g)=>(
+                <li key={g}>
+                  {g}
+                </li>
+              ))}
+            </ul>
+          </div>))}
+        </div>}
+
+      </div>
+    )
+
 }
+
 
 export default App;
