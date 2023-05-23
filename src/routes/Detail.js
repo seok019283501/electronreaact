f91fbb3629bcc688e6766f73d8574dacd738762e
seok019283501 <alsthr0338@gmail.com>
2023-05-23 21:44:46 +0900

    React Router

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,12 @@
+import {Link} from "react-router-dom";
+
 function Detail(){
-    return <h1>Detail</h1>
+    return (
+        <div>
+            <h1>Detail</h1>
+            <div><Link to="/">home</Link></div>
+        </div>
+    )
+    
 }
 export default Detail;
