3456b32a7f1392c5e2f26fa56adbbaa196b88d84
seok019283501 <alsthr0338@gmail.com>
2023-05-23 18:54:04 +0900

    To do list

--- src/main.js
+++ src/main.js
@@ -1,18 +1,35 @@
-const { app, BrowserWindow } = require('electron'); 
+const { app, BrowserWindow,Menu } = require('electron'); 
 const path = require('path'); 
 function createWindow () { 
     const win = new BrowserWindow({ 
-        width: 800, 
-        height: 600, 
+        width: 800,
+        height: 600,
+        minWidth: 800,
+        minHeight: 600,
         webPreferences: { 
         preload: path.join(__dirname, 'preload.js')
-        } 
-    }) 
+        }
+    })
     win.loadURL('http://localhost:3000'); 
 };
+const template = [
+    {
+        label:'File',
+        submenu:[
+            {
+                label : "open"
+            },
+            {
+                role:'toggleDevTools'
+            }
+        ]
+    }
+]
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
     app.whenReady().then(() => { 
     createWindow(); 
-}); 
+});
 app.on('window-all-closed', function () { 
     if (process.platform !== 'darwin') app.quit(); 
 });
